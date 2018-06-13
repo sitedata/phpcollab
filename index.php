1ad5e2f764acbb1c6fc2106466267d2977bb9ca5
Jeff Sittler <mindblender@gmail.com>
2018-06-12 18:28:32 -0700

    Removed commented out code at top

--- index.php
+++ index.php
@@ -3,12 +3,6 @@
 #Status page: 0
 #Path by root: index.php
 
-/**
- * Modification Log for 2.5
- *
- * 2008-11-20   -   Added setup detection (dab - norman77)
- *
- */
 /* Do Setup Check */
 if (!file_exists("includes/settings.php")) {
     header('Location: installation/setup.php');
